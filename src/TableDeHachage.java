b35c784553ac470a357c09bbf876721b5cb09e74
Stephano55 <kevinstephano14@gmail.com>
2024-05-18 11:59:58 +0300

    ajout d'une méthode pour povoir ajouter des valeurs à la table

--- src/TableDeHachage.java
+++ src/TableDeHachage.java
@@ -9,7 +9,33 @@ public class TableDeHachage<C,V> {
     private static class Entry<C,V> {
         final C cle;
         V valeur;
-        public Entry 
-        
+        public Entry(C cle, V valeur){
+            this.cle = cle;
+            this.valeur = valeur;
+        } 
+    }
+
+    public TableDeHachage(){
+        tab = new LinkedList[capInitiale];
+        size = 0;
+    }
+
+    private int hash(C cle){
+        return cle == null ? 0 : Math.abs(cle.hashCode())% tab.length;
+    }
+
+    public void entreValeur(C cle, V valeur){
+        int index = hash(cle);
+        if(tab[index]==null){
+            tab[index] = new LinkedList<>();
+        }
+        for(Entry<C,V> entry : tab[index]){
+            if(entry.cle.equals(cle)){
+                entry.valeur = valeur;
+                return;
+            }
+        }
+        tab[index].add(entry<>(cle,valeur));
+        size++;
     }
 }
