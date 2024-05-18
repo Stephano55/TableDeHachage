dd150386b6fe56bb749b141f93c2c4f124d53abc
Stephano55 <kevinstephano14@gmail.com>
2024-05-18 11:16:12 +0300

    création de la classe tableDeCharge

---
+++ src/TableDeHachage.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+import javax.swing.RowFilter.Entry;
+public class TableDeHachage<C,V> {
+    private static final int capInitiale = 16;
+    private LinkedList<Entry<C,V>>[] tab;
+    private int size;
+
+    private static class Entry<C,V> {
+        final C cle;
+        V valeur;
+        public Entry 
+        
+    }
+}
